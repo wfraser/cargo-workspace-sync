caaaeb290f2c7a58bbdfb918957f2fcf78747b7f
William R. Fraser <wfraser@codewise.org>
2023-09-10 22:15:32 -0700

    always move back root lockfile even if something fails with some workspace members

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
 use std::fs;
+use std::path::Path;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
 use anyhow::{bail, Context};
-use cargo_metadata::MetadataCommand;
+use cargo_metadata::{Metadata, MetadataCommand};
 use clap::Args;
 use clap::Parser;
 use clap::Subcommand;
@@ -33,6 +34,9 @@ struct SyncArgs {
     /// Pass the `--offline` flag to Cargo.
     #[arg(long)]
     offline: bool,
+
+    #[arg(skip)]
+    cargo_args: Vec<String>,
 }
 
 fn git_dirty_cmd() -> anyhow::Result<bool> {
@@ -57,10 +61,27 @@ fn git_dirty() -> bool {
         })
 }
 
+fn sync(meta: &Metadata, ws_lock: &Path, args: &SyncArgs) -> anyhow::Result<()> {
+    for pkg in meta.workspace_packages() {
+        let pkg_dir = pkg.manifest_path.parent().unwrap();
+        eprintln!("syncing {pkg_dir}");
+
+        env::set_current_dir(pkg_dir).context("failed to chdir into workspace member")?;
+
+        fs::copy(ws_lock, pkg_dir.join("Cargo.lock"))
+            .context("failed to copy root lockfile to workspace member")?;
+
+        // run some cargo command in member context to fix the lock file
+        MetadataCommand::new()
+            .other_options(args.cargo_args.clone())
+            .exec()
+            .with_context(|| format!("failed to run cargo metadata in workspace member {}", pkg.name))?;
+    }
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
-    let args = match ProgramArgs::parse().command {
-        Commands::WorkspaceSync(args) => args,
-    };
+    let Commands::WorkspaceSync(mut args) = ProgramArgs::parse().command;
 
     if !args.allow_dirty && git_dirty() {
         bail!("\
@@ -69,29 +90,25 @@ fn main() -> anyhow::Result<()> {
             or run again with the --allow-dirty flag.");
     }
 
-    let mut cargo_args = vec![];
     if args.offline {
-        cargo_args.push("--offline".into());
+        args.cargo_args.push("--offline".into());
     }
 
     let meta = MetadataCommand::new()
         .no_deps()
-        .other_options(cargo_args.clone())
+        .other_options(args.cargo_args.clone())
         .exec()
         .context("cargo metadata")?;
 
-    let ws_root = meta.workspace_root;
-
     // Can also get this by itself by running 'cargo locate-project --workspace'
-    let ws_toml = PathBuf::from(ws_root).join("Cargo.toml");
+    let ws_toml = PathBuf::from(&meta.workspace_root).join("Cargo.toml");
     let ws_toml_renamed = ws_toml.with_file_name("_Cargo_sync_temp.toml");
     let ws_lock = ws_toml.with_file_name("Cargo.lock");
 
     println!("workspace root toml: {ws_toml:?}");
 
-    let ws_members = meta.workspace_members;
-    if ws_members.len() < 2 {
-        eprintln!("workspace members: {ws_members:#?}");
+    if meta.workspace_members.len() < 2 {
+        eprintln!("workspace members: {:#?}", meta.workspace_members);
         bail!("no point in running this program without multiple workspace members");
     }
 
@@ -99,27 +116,11 @@ fn main() -> anyhow::Result<()> {
     fs::rename(&ws_toml, &ws_toml_renamed)
         .context("failed to rename workspace root Cargo.toml")?;
 
-    // foreach workspace member:
-    for pkgid in &ws_members {
-        let pkg = meta.packages.iter().find(|p| &p.id == pkgid).expect("unable to find package");
-        let pkg_dir = pkg.manifest_path.parent().unwrap();
-        eprintln!("syncing {pkgid}: {pkg_dir}");
-
-        env::set_current_dir(&pkg_dir).context("failed to chdir into workspace member")?;
-
-        fs::copy(&ws_lock, pkg_dir.join("Cargo.lock"))
-            .context("failed to copy root lockfile to workspace member")?;
-
-        // run some cargo command in member context to fix the lock file
-        MetadataCommand::new()
-            .other_options(cargo_args.clone())
-            .exec()
-            .with_context(|| format!("failed to run cargo metadata in workspace member {}", pkg.name))?;
-    }
+    let result = sync(&meta, &ws_lock, &args);
 
     // rename workspace Cargo.toml back
     fs::rename(&ws_toml_renamed, &ws_toml)
         .context("failed to rename back workspace root Cargo.toml")?;
 
-    Ok(())
+    result
 }
